cf03bfecd2a59c0ef3a28559ee2b4eb1454f2295
abcd <819284657@qq.com>
2015-01-16 18:15:08 +0800

    轮播

---
+++ carousel.js
@@ -0,0 +1,23 @@
+/**
+ * Created by azq on 2015/1/16.
+ */
+;(function(){
+    var sWidth = window.innerWidth;
+    var s = sWidth / 320;
+    document.body.style.zoom = s;
+    var wrap = document.getElementById("wrap");
+    var pic  = wrap.children;
+    var index = 0;
+    var imgW = 160;
+    document.getElementById("pre").addEventListener("click",function(){
+        index++;
+        wrap.style.transform = "translateX("+-imgW+"px)";
+        wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
+        setTimeout(function(){
+            var li = pic[0];
+            wrap.removeChild(pic[0]);
+            wrap.appendChild(li);
+        },1000)
+
+    },false)
+})()
