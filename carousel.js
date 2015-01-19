757546dd88f8313fe61be5a49d3e5364daa45dd8
abcd <819284657@qq.com>
2015-01-19 17:52:31 +0800

    增加触摸事件

--- carousel.js
+++ carousel.js
@@ -8,8 +8,46 @@
     var pic  = wrap.children;
     var n = pic.length;
     var imgW = 160;
-
+    var startX = 0;
+    var D = 0;
     document.getElementById("next").addEventListener("click",function(){
+        moveRight();
+    },false);
+    document.getElementById("pre").addEventListener("click",function(){
+        moveLeft();
+    },false);
+    document.getElementById("show").addEventListener("touchstart",function(e){
+        e.preventDefault();
+        startX = event.targetTouches[0].pageX;
+    },false);
+    document.getElementById("show").addEventListener("touchmove",function(e){
+        e.preventDefault();
+        var X = event.targetTouches[0].pageX;
+        D = startX - X;
+        console.log(D);
+    },false);
+    document.getElementById("show").addEventListener("touchend",function(e){
+        e.preventDefault();
+        console.log(D);
+        if (D<=0){
+            moveLeft();
+        }
+        if (D>0){
+            moveRight();
+        }
+    },false);
+    function moveLeft(){
+        var li = pic[n-1];
+        wrap.removeChild(pic[n-1]);
+        wrap.insertBefore(li,pic[0]);
+        wrap.style.transform = "translateX("+(-imgW)+"px)";
+        wrap.style.webkitTransition = "none";
+        setTimeout(function(){
+            wrap.style.transform = "translateX(0)";
+            wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
+        },100);
+    }
+    function moveRight(){
         wrap.style.transform = "translateX("+(-imgW)+"px)";
         wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
         setTimeout(function(){
@@ -19,16 +57,5 @@
             wrap.style.webkitTransition = "none";
             wrap.style.transform = "translateX(0)";
         },1000)
-    },false);
-    document.getElementById("pre").addEventListener("click",function(){
-        var li = pic[n-1];
-        wrap.removeChild(pic[n-1]);
-        wrap.insertBefore(li,pic[0]);
-        wrap.style.transform = "translateX("+(-imgW)+"px)";
-        wrap.style.webkitTransition = "none";
-    setTimeout(function(){
-        wrap.style.transform = "translateX(0)";
-        wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
-    },100)
-    },false)
+    }
 })();
