87ceed1ef3df8930d3a0c9cdfd3ce2abc05ae423
abcd <819284657@qq.com>
2015-01-19 09:38:29 +0800

    变量名s更改

--- carousel.js
+++ carousel.js
@@ -3,22 +3,21 @@
  */
 ;(function(){
     var sWidth = window.innerWidth;
-    var s = sWidth / 320;
-    document.body.style.zoom = s;
+    var scale = sWidth / 320;
+    document.body.style.zoom = scale;
     var wrap = document.getElementById("wrap");
     var pic  = wrap.children;
     var index = 0;
     var imgW = 160;
+
     document.getElementById("pre").addEventListener("click",function(){
         index++;
-        wrap.style.transform = "translateX("+-imgW+"px)";
+        wrap.style.transform = "translateX("+(-imgW)+"px)";
         wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
         setTimeout(function(){
             var li = pic[0];
             wrap.removeChild(pic[0]);
             wrap.appendChild(li);
         },1000)
-
-
     },false)
-})()
+})();
