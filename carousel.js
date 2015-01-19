ceac5c9d49e72824f2af185aade9dddb81545b13
abcd <819284657@qq.com>
2015-01-19 17:15:51 +0800

    添加点击pre向左滚动

--- carousel.js
+++ carousel.js
@@ -3,21 +3,38 @@
  */
 ;(function(){
     var sWidth = window.innerWidth;
-    var scale = sWidth / 320;
-    document.body.style.zoom = scale;
+    document.body.style.zoom = sWidth / 320;
     var wrap = document.getElementById("wrap");
     var pic  = wrap.children;
-    var index = 0;
+    var n = pic.length;
     var imgW = 160;
 
-    document.getElementById("pre").addEventListener("click",function(){
-        index++;
+    document.getElementById("next").addEventListener("click",function(){
         wrap.style.transform = "translateX("+(-imgW)+"px)";
         wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
         setTimeout(function(){
             var li = pic[0];
             wrap.removeChild(pic[0]);
             wrap.appendChild(li);
+            wrap.style.webkitTransition = "none";
+            wrap.style.transform = "translateX(0)";
         },1000)
+    },false);
+    document.getElementById("pre").addEventListener("click",function(){
+        var li = pic[n-1];
+        wrap.removeChild(pic[n-1]);
+        wrap.insertBefore(li,pic[0]);
+        wrap.style.transform = "translateX("+(-imgW)+"px)";
+        wrap.style.webkitTransition = "none";
+setTimeout(function(){
+    wrap.style.transform = "translateX(0)";
+    wrap.style.webkitTransition = "all 1s cubic-bezier(1,1,1,1) 0";
+},100)
+
+
+
+
+
+
     },false)
 })();
